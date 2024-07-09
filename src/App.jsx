e12570de950cd57670ff9eff25cd437d7e379b3c
unknown <amaan2228web@gmail.com>
2024-07-09 12:59:29 +0530

    React Router Navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
-import {InputTask} from './Components/InputTask'
+import { InputTask } from './Components/InputTask'
 import { DisplayTask } from './Components/DisplayTask'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Layout} from './Pages/Layout'
+import {Home} from './Pages/Home'
+import { Details } from './Pages/Details'
 
 export const App = () => {
   return (
     <div>
-      <InputTask />
-      <DisplayTask />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Layout />} />
+          <Route index element={<Home /> } />
+          <Route path='details' element={<Details /> } />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
